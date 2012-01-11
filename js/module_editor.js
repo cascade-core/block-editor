89bf7a94736ea22aac071b1ac2cd19265ae598c1
Josef Kufner <jk@frozen-doe.net>
2012-01-12 00:42:13 +0100

    Initial commit

---
+++ js/module_editor.js
@@ -0,0 +1,97 @@
+/*
+ * Module Editor Widget
+ *
+ * Copyright (c) 2011, Josef Kufner  <jk@frozen-doe.net>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the author nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+(function($) {
+	
+	$.fn.moduleEditorWidget = function() {
+		this.each(function() {
+			var $this = $(this);
+
+			if (!$this.hasClass('module_editor_widget') || this.tagName.toLowerCase() != 'textarea') {
+				if (console) {
+					console.error('Cannot convert this element to Module Editor widget. It must be textarea with \'module_editor_widget\' class.');
+				}
+				return;
+			}
+
+			var widget = $('<div class=\"module_editor_widget widget\"></div>');
+			widget.textarea = $this;
+			widget.css({
+				position:	'relative',
+				overflow:	'auto',
+				height:		$this.css('height'),
+				width:		$this.css('width')
+			});
+			widget.insertBefore($this);
+
+			widget.updateFromTextarea = function() {
+				var d = eval('(' + this.textarea.val() + ')');
+				var geometry = {};
+
+				if ('geometry' in d) {
+					geometry = d['geometry'];
+				}
+
+				for (var i in d) {
+					if (i.substr(0, 7) == 'module:') {
+						var id = i.substr(7);
+						var connections = d[i];
+
+						var m = $('<div class="module_editor_widget__module"></div>').css({
+								'width': 'auto',
+								'float': 'left',
+								'border': '1px solid grey',
+								'margin': '1ex'
+							});
+						var t = $('<table width="100%"></table>');
+						t.append($('<tr></tr>').append($('<th colspan="3" style="text-align:center;"></th>').text(id)));
+						t.append($('<tr></tr>')
+								.append($('<td width="45%" style="text-align:left;"></td>').text('input'))
+								.append($('<td width="10%"></td>'))
+								.append($('<td width="45%" style="text-align:right;"></td>').text('output'))
+								);
+						m.append(t);
+						this.append(m);
+					}
+				}
+			};
+
+			widget.updateFromTextarea();
+		});
+		return this;
+	};
+
+	$(document).ready(function() {
+		$('textarea.module_editor_widget').moduleEditorWidget();
+	});
+
+})(jQuery);
+
