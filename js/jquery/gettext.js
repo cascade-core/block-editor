2b1610a4b654824237758f6d6521fb5b3a9505b4
Josef Kufner <jk@frozen-doe.net>
2012-03-09 13:48:51 +0100

    gettext stub

---
+++ js/jquery/gettext.js
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2011, Josef Kufner  <jk@frozen-doe.net>
+ * All rights reserved.
+ *
+ * Licensed under MIT license.
+ *
+ */
+
+if (typeof(_) == 'undefined') {
+	function _(str) {
+		return str;
+	}
+}
+
