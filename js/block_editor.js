52386b7edb70c56136a845a448c833239b8d263f
Josef Kufner <jk@frozen-doe.net>
2012-03-09 13:49:11 +0100

    Input settings dialog

--- js/block_editor.js
+++ js/block_editor.js
@@ -31,246 +31,6 @@
  */
 
 (function($) {
-
-	// Block
-	function Block(id, block, doc_link, onChange)
-	{
-		this.id = id;
-		this.block = block;
-		this.column = 0;
-		this.inputs = {};
-		this.outputs = {};
-		this.input_divs = {};	// one div for each input
-		this.output_divs = {};	// one div for each output
-		this.connections = {};	// lines from inputs to other blocks
-		this.x = null;
-		this.y = null;
-
-		this.onChange = onChange; // callback when anything changes
-
-		this.widget = $('<div class="block_editor_widget__block"></div>');
-		this.widget.addClass(getBlockClass(block));
-
-		this.inputs_holder = $('<td class="block_editor_widget__in"></td>');
-		this.outputs_holder = $('<td class="block_editor_widget__out"></td>');
-
-		this.widget.append($('<table></table>')
-			.append($('<tr></tr>').append($('<th colspan="2"></th>')
-				.append($('<div class="block_editor_widget__block_id"></div>').text(id))
-				.append($('<div class="block_editor_widget__block_name"></div>')
-					.append($('<a></a>').text(block)
-						.attr('href', doc_link + block)
-						.attr('target', '_blank')
-					))))
-			.append($('<tr></tr>').append(this.inputs_holder).append(this.outputs_holder))
-		);
-		// b.append($('<a href="#" class="block_editor_widget__add_input">add input</a>'));
-
-		this.addInput = function(name, value) {
-			this.inputs[name] = value;
-
-			var div;
-			if (name in this.input_divs) {
-				div = this.input_divs[name];
-			} else {
-				div = $('<div></div>').text(name);
-				this.input_divs[name] = div;
-				this.inputs_holder.append(div);
-			}
-			if (value && typeof(value) == 'object') {
-				div.attr('data-connection', json_encode(value));
-			} else {
-				div.attr('data-value', value);
-			}
-		};
-
-		this.addInputs = function(list) {
-			for (var i in list) {
-				if (i[0] != '.') {
-					this.addInput(i, list[i]);
-				}
-			}
-		}
-
-		this.addOutput = function(name, value) {
-			this.outputs[name] = value;
-
-			var div;
-			if (name in this.output_divs) {
-				div = this.input_divs[name];
-			} else {
-				div = $('<div></div>').text(name);
-				this.outputs_holder.append(div);
-				this.output_divs[name] = div;
-			}
-		};
-
-		this.addOutputs = function(list) {
-			for (var i in list) {
-				if (i[0] != '.') {
-					this.addOutput(i, list[i]);
-				}
-			}
-		};
-
-		this.setPosition = function(x, y) {
-			if (x != null) {
-				this.x = Math.round(x);
-				this.widget.css('left', this.x);
-			}
-			if (y != null) {
-				this.y = Math.round(y);
-				this.widget.css('top', this.y);
-			}
-			this.onChange();
-		}
-
-		this.updateColumn = function(serial) {
-			if (serial == this.column_serial) {
-				return this.column;
-			} else {
-				this.column_serial = serial;
-			}
-
-			max_column = 0;
-			for (var ci in this.connections) {
-				var b = this.connections[ci].block;
-				b.updateColumn(serial);
-				if (b.column >= max_column) {
-					max_column = b.column;
-				}
-			}
-			this.column = max_column + 1;
-		};
-
-		this.positionOnCanvas = function(element)
-		{
-			var s = element.position();
-			element.parentsUntil('.block_editor_widget__canvas').each(function() {
-				$this = $(this);
-				if ($this.css('position') == 'absolute') {
-					var p = $this.position();
-					s.left += p.left;
-					s.top += p.top;
-				}
-			});
-			return s;
-		}
-
-		// Create or update all connections
-		this.connect = function(blocks, raph) {
-			for (var i in this.inputs) {
-				var target = [this.id, i];
-				var have_input = this.inputs[i] && typeof(this.inputs[i]) == 'object';
-				var input_fn = have_input && this.inputs[i][0][0] == ':' ? this.inputs[i][0] : null;
-
-				/*
-				console.log('--');
-				console.log('Target:', target, have_input ? '... with input.' : '');
-				console.log('Source:', this.inputs[i]);
-				console.log('Input function:', [input_fn]);
-				// */
-
-				for (var ii = input_fn == null ? 0 : 1; have_input && ii < this.inputs[i].length; ii++) {
-					var source = have_input ? this.inputs[i][ii].split(':') : null;
-					var ci = i + ':' + ii;
-
-					//console.log(ci, this.inputs[i], '->', source);
-
-					var line_path;
-					var arrow_path;
-
-					if (source) {
-						// update or create ... we need path anyway
-
-						if (source[0] == '') {
-							continue;
-						}
-
-						// source
-						var s_div = blocks[source[0]].output_divs[source[1]];
-						if (!s_div) {
-							if ('*' in blocks[source[0]].outputs) {
-								blocks[source[0]].addOutput(source[1]);
-								s_div = blocks[source[0]].output_divs[source[1]];
-							} else {
-								continue;
-							}
-						}
-						var s = this.positionOnCanvas(s_div);
-						var sh = Math.round(s_div.height() / 2);
-						var srcX = s.left + s_div.outerWidth() + 4;
-						var srcY = s.top + sh;
-
-						// destination
-						var d_div = this.input_divs[i];
-						if (!d_div) {
-							continue;
-						}
-						var d = this.positionOnCanvas(d_div);
-						var dh = Math.round(d_div.height() / 2);
-						var dstX = d.left;
-						var dstY = d.top + dh;
-
-						// line path
-						var c = Math.abs(srcX - dstX) / 2;
-						line_path = [
-							'M', srcX, srcY,
-							'C', (srcX + c), srcY,  (dstX - c), dstY,  (dstX - dh), dstY,
-							'L', dstX + ' ' + dstY
-						].join(',')
-
-						// arrow path
-						arrow_path = [
-							'M', dstX, dstY,
-							'L', (dstX - dh), (dstY - (dh - 1)),
-							'L', (dstX - 0.7 * dh), dstY,
-							'L', (dstX - dh), (dstY + (dh - 1)),
-							'Z'
-						].join(',');
-					}
-
-					if (ci in this.connections) {
-						if (source) {
-							// update
-							this.connections[ci].line.attr('path', line_path);
-							this.connections[ci].arrow.attr('path', arrow_path);
-						} else {
-							// remove
-							this.connections[ci].line.remove();
-							this.connections[ci].arrow.remove();
-							delete this.connections[ci];
-						}
-					} else if (source) {
-						// create
-						//console.log('Connection:', source, '->', target);
-
-						this.connections[ci] = {
-							block: blocks[source[0]],
-							arrow: raph.path(arrow_path).attr({
-								'stroke': '#000',
-								'fill': '#000'
-							}),
-							line: raph.path(line_path).attr({
-								'stroke': '#000',
-								'stroke-width': 1.4
-							})
-						};
-					}
-				}
-			}
-			raph.safari();
-			//console.log(this.connections);
-		};
-	}
-
-	// Calculate block prefix from it's name
-	function getBlockClass(block)
-	{
-		return 'block_editor_widget__block__' + block.replace(/\/[^\/]*$/, '').replace('/', '__');
-	}
-
-
 	$.fn.blockEditorWidget = function() {
 		this.each(function() {
 			var canvas_width = 4000;
@@ -279,9 +39,301 @@
 			var spacing_horiz = 80;		// Horizontal spacing between blocks.
 			var pan_speed = 2;		// Mouse pan multiplication (when mouse moves by 1 px, canvas scrolls for pan_speed px).
 
+			// Block
+			function Block(id, block, doc_link, onChange)
+			{
+				this.id = id;
+				this.block = block;
+				this.column = 0;
+				this.inputs = {};
+				this.outputs = {};
+				this.input_divs = {};	// one div for each input
+				this.output_divs = {};	// one div for each output
+				this.connections = {};	// lines from inputs to other blocks
+				this.x = null;
+				this.y = null;
+
+				this.onChange = onChange; // callback when anything changes
+
+				this.widget = $('<div class="block_editor_widget__block"></div>');
+				this.widget.addClass(getBlockClass(block));
+
+				this.inputs_holder = $('<td class="block_editor_widget__in"></td>');
+				this.outputs_holder = $('<td class="block_editor_widget__out"></td>');
+
+				this.widget.append($('<table></table>')
+					.append($('<tr></tr>').append($('<th colspan="2"></th>')
+						.append($('<div class="block_editor_widget__block_id"></div>').text(id))
+						.append($('<div class="block_editor_widget__block_name"></div>')
+							.append($('<a></a>').text(block)
+								.attr('href', doc_link + block)
+								.attr('target', '_blank')
+							))))
+					.append($('<tr></tr>').append(this.inputs_holder).append(this.outputs_holder))
+				);
+
+				this.addInput = function(name, value) {
+					this.inputs[name] = value;
+
+					var div;
+					if (name in this.input_divs) {
+						div = this.input_divs[name];
+					} else {
+						div = $('<a></a>').text(name);
+						div.attr('href', '#');
+						div.click((function (t, name) { return function() { t.editInput(name); return false; };})(this, name));
+						this.input_divs[name] = div;
+						this.inputs_holder.append(div);
+					}
+				};
+
+				this.addInputs = function(list) {
+					for (var i in list) {
+						if (i[0] != '.') {
+							this.addInput(i, list[i]);
+						}
+					}
+				}
+
+				this.addOutput = function(name, value) {
+					this.outputs[name] = value;
+
+					var div;
+					if (name in this.output_divs) {
+						div = this.input_divs[name];
+					} else {
+						div = $('<div></div>').text(name);
+						this.outputs_holder.append(div);
+						this.output_divs[name] = div;
+					}
+				};
+
+				this.addOutputs = function(list) {
+					for (var i in list) {
+						if (i[0] != '.') {
+							this.addOutput(i, list[i]);
+						}
+					}
+				};
+
+				this.editInput = function(name) {
+					var d = createDialog(this.widget, this.id + ":" + name);
+					var type = $('<select class="focus"></select>');
+					var ta = $('<textarea></textarea>');
+					var desc = $('<div></div>');
+					var set = $('<input>').val(_('Set input')).attr('type', 'submit');
+					d.append(type);
+					d.append(desc);
+					d.append(ta);
+					d.append(set);
+
+					type.append($('<option></option>').text(_('Default state')).attr('value', 'default'));
+					type.append($('<option></option>').text(_('Connection:')).attr('value', 'connection'));
+					type.append($('<option></option>').text(_('Boolean value:')).attr('value', 'bool'));
+					type.append($('<option></option>').text(_('Integer value:')).attr('value', 'int'));
+					type.append($('<option></option>').text(_('String value:')).attr('value', 'string'));
+
+					var desc_text = {
+						'default':	[ false, _('Input is set to it\'s default state, as specified in documentation.') ],
+						'connection':	[ true,  _('Input is connected to these outputs: (Syntax: "block:output", one connection per line.)') ],
+						'bool':		[ true,  _('Input is set to this boolean value (true or false):') ],
+						'int':		[ true,  _('Input is set to this number:') ],
+						'string':	[ true,  _('Input is set to this text:') ]
+					};
+
+					var update_desc = function() {
+						var d = desc_text[type.val()];
+						ta.css('display', d[0] ? 'block' : 'none');
+						desc.html(d[1]);
+					};
+
+					type.change(update_desc).keyup(update_desc);
+
+					console.log(this.inputs[name], typeof(this.inputs[name]));
+
+					if (!(name in this.inputs) || this.inputs[name] === null) {
+						type.val('default');
+					} else if (typeof(this.inputs[name]) == 'object') {
+						type.val('connection');
+						ta.val(this.inputs[name].join('\n'));
+					} else if (typeof(this.inputs[name]) == 'boolean') {
+						type.val('bool');
+						ta.val(this.inputs[name]);
+					} else {
+						type.val('string');
+						ta.val(this.inputs[name]);
+					}
+
+					ta.autogrow();
+					update_desc();
+
+					set.click(function() {
+						d.close();
+						return false;
+					});
+				};
+
+				this.setPosition = function(x, y) {
+					if (x != null) {
+						this.x = Math.round(x);
+						this.widget.css('left', this.x);
+					}
+					if (y != null) {
+						this.y = Math.round(y);
+						this.widget.css('top', this.y);
+					}
+					this.onChange();
+				}
+
+				this.updateColumn = function(serial) {
+					if (serial == this.column_serial) {
+						return this.column;
+					} else {
+						this.column_serial = serial;
+					}
+
+					max_column = 0;
+					for (var ci in this.connections) {
+						var b = this.connections[ci].block;
+						b.updateColumn(serial);
+						if (b.column >= max_column) {
+							max_column = b.column;
+						}
+					}
+					this.column = max_column + 1;
+				};
+
+				this.positionOnCanvas = function(element)
+				{
+					var s = element.position();
+					element.parentsUntil('.block_editor_widget__canvas').each(function() {
+						$this = $(this);
+						if ($this.css('position') == 'absolute') {
+							var p = $this.position();
+							s.left += p.left;
+							s.top += p.top;
+						}
+					});
+					return s;
+				}
+
+				// Create or update all connections
+				this.connect = function(blocks, raph) {
+					for (var i in this.inputs) {
+						var target = [this.id, i];
+						var have_input = this.inputs[i] && typeof(this.inputs[i]) == 'object';
+						var input_fn = have_input && this.inputs[i][0][0] == ':' ? this.inputs[i][0] : null;
+
+						/*
+						console.log('--');
+						console.log('Target:', target, have_input ? '... with input.' : '');
+						console.log('Source:', this.inputs[i]);
+						console.log('Input function:', [input_fn]);
+						// */
+
+						for (var ii = input_fn == null ? 0 : 1; have_input && ii < this.inputs[i].length; ii++) {
+							var source = have_input ? this.inputs[i][ii].split(':') : null;
+							var ci = i + ':' + ii;
+
+							//console.log(ci, this.inputs[i], '->', source);
+
+							var line_path;
+							var arrow_path;
+
+							if (source) {
+								// update or create ... we need path anyway
+
+								if (source[0] == '') {
+									continue;
+								}
+
+								// source
+								var s_div = blocks[source[0]].output_divs[source[1]];
+								if (!s_div) {
+									if ('*' in blocks[source[0]].outputs) {
+										blocks[source[0]].addOutput(source[1]);
+										s_div = blocks[source[0]].output_divs[source[1]];
+									} else {
+										continue;
+									}
+								}
+								var s = this.positionOnCanvas(s_div);
+								var sh = Math.round(s_div.height() / 2);
+								var srcX = s.left + s_div.outerWidth() + 4;
+								var srcY = s.top + sh;
+
+								// destination
+								var d_div = this.input_divs[i];
+								if (!d_div) {
+									continue;
+								}
+								var d = this.positionOnCanvas(d_div);
+								var dh = Math.round(d_div.height() / 2);
+								var dstX = d.left;
+								var dstY = d.top + dh;
+
+								// line path
+								var c = Math.abs(srcX - dstX) / 2;
+								line_path = [
+									'M', srcX, srcY,
+									'C', (srcX + c), srcY,  (dstX - c), dstY,  (dstX - dh), dstY,
+									'L', dstX + ' ' + dstY
+								].join(',')
+
+								// arrow path
+								arrow_path = [
+									'M', dstX, dstY,
+									'L', (dstX - dh), (dstY - (dh - 1)),
+									'L', (dstX - 0.7 * dh), dstY,
+									'L', (dstX - dh), (dstY + (dh - 1)),
+									'Z'
+								].join(',');
+							}
+
+							if (ci in this.connections) {
+								if (source) {
+									// update
+									this.connections[ci].line.attr('path', line_path);
+									this.connections[ci].arrow.attr('path', arrow_path);
+								} else {
+									// remove
+									this.connections[ci].line.remove();
+									this.connections[ci].arrow.remove();
+									delete this.connections[ci];
+								}
+							} else if (source) {
+								// create
+								//console.log('Connection:', source, '->', target);
+
+								this.connections[ci] = {
+									block: blocks[source[0]],
+									arrow: raph.path(arrow_path).attr({
+										'stroke': '#000',
+										'fill': '#000'
+									}),
+									line: raph.path(line_path).attr({
+										'stroke': '#000',
+										'stroke-width': 1.4
+									})
+								};
+							}
+						}
+					}
+					raph.safari();
+					//console.log(this.connections);
+				};
+			}
+
+			// Calculate block prefix from it's name
+			function getBlockClass(block)
+			{
+				return 'block_editor_widget__block__' + block.replace(/\/[^\/]*$/, '').replace('/', '__');
+			}
+
 			var textarea = $(this);
 			var blocks = {};
 			var doc_link = textarea.attr('data-doc_link');
+			var current_dialog = null;
 
 			if (!textarea.hasClass('block_editor_widget') || this.tagName.toLowerCase() != 'textarea') {
 				if (console) {
@@ -296,6 +348,43 @@
 			widget.disableSelection();
 			widget.insertBefore(textarea);
 			//textarea.css('display', 'none !important');
+
+			// Canvas dialog factory
+			function createDialog(holder, title)
+			{
+				if (current_dialog) {
+					current_dialog.remove();
+				}
+
+				current_dialog = $('<div></div>').addClass('block_editor_widget__dialog');
+				current_dialog.hide();
+				widget.append(current_dialog);
+
+				current_dialog.close = function() {
+					current_dialog.hide('drop', { direction: 'up'}, 200,
+							(function(current_dialog) { return current_dialog.remove; })(current_dialog));
+					current_dialog = null;
+					return false;
+				};
+
+				var title_element = $('<div class="block_editor_widget__dialog_title"></div>');
+				title_element.text(title);
+				title_element.append($('<a href="#">&times;</a>').click(current_dialog.close));
+				current_dialog.append(title_element);
+
+				current_dialog.show('drop', { direction: 'up' }, 200, function() {
+					current_dialog.find('.focus').focus();
+					current_dialog.keydown(function(ev) {
+						if(ev.keyCode == 27) {
+							return current_dialog.close();
+						} else {
+							return true;
+						}
+					});
+				});
+
+				return current_dialog;
+			};
 
 			// Create canvas
 			var canvas = $('<div class="block_editor_widget__canvas"></div>');
